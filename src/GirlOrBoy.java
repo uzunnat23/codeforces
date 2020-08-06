26a345f5037925858aae920b01da9206e53f2a42
uzunnat23 <uzunnat23@gmail.com>
2020-08-06 18:59:47 +0300

    Girl or boy?

---
+++ src/GirlOrBoy.java
@@ -0,0 +1,28 @@
+/*
+    Если количество различных символов в имени пользователя нечетное, тогда пользователь мужского пола, иначе — женского.
+    Вам дана строка, обозначающая имя пользователя, помогите нашему герою определить по ней пол пользователя по описанному методу.
+    Если пользователь оказался женского пола по методу нашего героя, выведите «CHAT WITH HER!» , иначе, выведите «IGNORE HIM!»
+*/
+import java.util.Scanner;
+
+public class GirlOrBoy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        char[] strArr = str.toCharArray();
+        int k = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (strArr[i] == strArr[j]) {
+                    k++;
+                    break;
+                }
+            }
+        }
+
+        if ((str.length() - k) % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else System.out.println("IGNORE HIM!");
+    }
+}
